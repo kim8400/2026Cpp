8557b02b307bf2e2a758938d39eb1e29da5bc112
KIM SUNHYEOK <202230106@daelim.ac.kr>
2026-03-19 14:40:43 +0900

    week03.v0.4

---
+++ .venv/week02.py
@@ -0,0 +1,4 @@
+import statistics # 통계모둘 (각종 통계함수를 제공)
+scores = [100,97,88,91]
+average = statistics.mean(scores)
+print(average)
---
+++ week03.py
@@ -0,0 +1,7 @@
+import  numpy # 써드파티 라이브러리, c 배열 기반으로 대규모 데이터 처리에 접합.매우 빠름
+
+
+# 통계모듈 (각종 통계함수를 제공), 파이썬으로
+scores = [100,97,88,91]
+average = numpy.mean(scores)
+print(average)
